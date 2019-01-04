aa1e294dc1a364e04ce00ea15310764058258147
Biradar, Pravin <PravinBiradar@nexsystech.com>
2019-01-04 17:10:55 -0500

    Added new List which contains images and text as well

--- projects_list/lib/main.dart
+++ projects_list/lib/main.dart
@@ -2,19 +2,41 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(new ProjectsListApp());
 
-class ProjectsListApp  extends StatelessWidget {
+class ProjectsListApp extends StatelessWidget {
   @override
-    Widget build(BuildContext context) {
-      return new MaterialApp(
+  Widget build(BuildContext context) {
+    return new MaterialApp(
         title: "Project List",
         home: new Scaffold(
           appBar: new AppBar(
             title: new Text("Projects List"),
           ),
-          body: new Center(
-            child: Text("Home"),
+          body: new HomeWidget(),
+        ));
+  }
+}
+
+class HomeWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new ListView.builder(
+      itemCount: 20,
+      itemBuilder: (context, rowNumber) {
+        var num = rowNumber + 1;
+        return new Container(
+          padding: new EdgeInsets.all(16.0),
+          child: new Column(
+            children: <Widget>[
+              new Image.network(
+                "https://raw.githubusercontent.com/flutter/website/master/src/_includes/code/layout/lakes/images/lake.jpg",
+              ),
+              new Container(height: 10.0,),
+              new Text("Project $num"),
+              new Divider(),
+            ],
           ),
-        ) 
-      );
-    }
+        );
+      },
+    );
+  }
 }
